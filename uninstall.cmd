c35d91fd97f84a73d9f0dd1387d6a159b4c092a8
Moose <ryan.schultz915@outlook.com>
2024-05-15 07:18:31 -0500

    Create uninstall.cmd

---
+++ uninstall.cmd
@@ -0,0 +1,3 @@
+rem Example: msiexec.exe /x {Enter-Your-ID-String-Here} UNIQUE_NAME="bel*" QBFULLNAME="QuickBooks Enterprise Solutions: ?????" ADDREMOVE=1 /passive
+
+msiexec.exe /x {B9BE758E-50B5-4BA7-987B-63184123AA1A} UNIQUE_NAME="belcontractor" QBFULLNAME="QuickBooks Enterprise Solutions: Contractor Edition 22.0" ADDREMOVE=1 /passive
